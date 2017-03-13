ee328200191ead0c49b1acbf099ea81885b9f850
Patrik Bak <bakyx@centrum.sk>
2017-03-13 18:32:10 +0100

    Connecting the generator to the new dao

--- ShiftScheduleDataAccess/Dao/FileDao/FolderConstants.cs
+++ ShiftScheduleDataAccess/Dao/FileDao/FolderConstants.cs
@@ -1,12 +1,11 @@
 ﻿namespace ShiftScheduleDataAccess.Dao.FileDao
 {
-    public static class FolderConstants
+    internal static class FolderConstants
     {
         public const string RequirementsFileName = "requirements";
         public const string ResultingScheduleFileName = "result";
         public const string RequirementsFulfillingStatsFileName = "statistics";
         public const string FileExtensions = "txt";
         public const string OutputFolderName = "Output";
-        public const string SetFolderName = "Set";
     }
 }
